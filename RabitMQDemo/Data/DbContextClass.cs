198727be873310db3c3523138d2834b80cbb3132
Rakesh Kumar <gpaswan1976@gmail.com>
2023-04-05 16:17:38 +0530

    Add project files.

---
+++ RabitMQDemo/Data/DbContextClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabitMQDemo.Models;
+using System.Collections.Generic;
+
+namespace RabitMQDemo.Data
+{
+    public class DbContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DbContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
---
+++ RabitMQDemo/RabbitMQ/IRabitMQProducer.cs
@@ -0,0 +1,7 @@
+﻿namespace RabitMQDemo.RabbitMQ
+{
+    public interface IRabitMQProducer
+    {
+        public void SendProductMessage<T>(T message);
+    }
+}
